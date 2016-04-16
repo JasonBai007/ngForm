e365e461d8bd1c61db88a313d913b5dc0ad74f88
jasonbai <1105843831@qq.com>
2016-04-16 17:07:20 +0800

    优化表单，添加图标

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,23 @@ var ngForm = angular.module('myApp',['ui.bootstrap']);
 
 ngForm.controller('myCtrl',['$scope',function($scope){
 
-       
-        
+	$scope.records = [];
+	$scope.submit = function() {
+	console.log($scope.user);
+	// $scope.user.handle = "";
+		
+		var item = angular.copy($scope.user);
+		$scope.records.push(item);
+	}
+
+	$scope.reset = function() {
+		$scope.user = {};
+	}
+
+	$scope.open = function() {
+		alert('haha');
+	}
+
+
+
 }]);
