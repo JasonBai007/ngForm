1431db69d761872c2eb4b49f12c63304d00a7e3c
jasonbai <1105843831@qq.com>
2016-04-17 14:55:22 +0800

    完善表单验证

--- js/controller.js
+++ js/controller.js
@@ -1,45 +1,38 @@
 
 var ngForm = angular.module('myApp',['ui.bootstrap']);  
 
-ngForm.controller('myCtrl',['$scope','$uibModal',function($scope,$uibModal){
+ngForm.controller('myCtrl',['$scope',function($scope){
 
+	// 初始化
+	$scope.user = {};
 	$scope.records = [];
-	$scope.areas = ["海淀","朝阳","丰台","昌平","通州","大兴","亦庄","密云"];
-	$scope.submit = function() {
-	console.log($scope.user);
-		
+	$scope.areas = ["海淀","朝阳","丰台","昌平","通州","大兴","亦庄","密云","顺义","房山"];
+
+	// 提交当条记录
+	$scope.submit = function() {		
 		var item = angular.copy($scope.user);
 		$scope.records.push(item);
+		$scope.user = {};
 	}
 
+	// 重置表单
 	$scope.reset = function() {
 		$scope.user = {};
 	}
 
-	// 配置删除弹框
-	$scope.open = function(size) {
-		var modalInstance = $uibModal.open({
-			animation: true,
-			templateUrl: 'myModalContent.html',
-			controller: 'ModalInstanceCtrl',
-			size: size			
-		});
+	// 编辑记录
+	$scope.edit = function ($index) {
+		var i = $index;
+		$scope.user = angular.copy($scope.records[i]);
+	}
+
+	// 删除记录
+	$scope.del = function($index) {
+		var i = $index;
+		$scope.records.splice(i,1);
 	}
 
 
 
 }]);
 
-// 弹出模块示例控制器
-ngForm.controller('ModalInstanceCtrl',['$scope','$uibModalInstance',function($scope, $uibModalInstance){
-	$scope.ok = function () {
-	   $uibModalInstance.close();
-	 };
-
-	$scope.cancel = function () {
-	  $uibModalInstance.dismiss('cancel');
-	};
-
-
-
-}]);
