a08b15e79a34825fa3530b488c59c16fa1b85bcc
jasonbai <1105843831@qq.com>
2016-04-17 11:23:35 +0800

    大范围完善表单

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,12 @@
 
 var ngForm = angular.module('myApp',['ui.bootstrap']);  
 
-ngForm.controller('myCtrl',['$scope',function($scope){
+ngForm.controller('myCtrl',['$scope','$uibModal',function($scope,$uibModal){
 
 	$scope.records = [];
+	$scope.areas = ["海淀","朝阳","丰台","昌平","通州","大兴","亦庄","密云"];
 	$scope.submit = function() {
 	console.log($scope.user);
-	// $scope.user.handle = "";
 		
 		var item = angular.copy($scope.user);
 		$scope.records.push(item);
@@ -16,9 +16,29 @@ ngForm.controller('myCtrl',['$scope',function($scope){
 		$scope.user = {};
 	}
 
-	$scope.open = function() {
-		alert('haha');
+	// 配置删除弹框
+	$scope.open = function(size) {
+		var modalInstance = $uibModal.open({
+			animation: true,
+			templateUrl: 'myModalContent.html',
+			controller: 'ModalInstanceCtrl',
+			size: size			
+		});
 	}
+
+
+
+}]);
+
+// 弹出模块示例控制器
+ngForm.controller('ModalInstanceCtrl',['$scope','$uibModalInstance',function($scope, $uibModalInstance){
+	$scope.ok = function () {
+	   $uibModalInstance.close();
+	 };
+
+	$scope.cancel = function () {
+	  $uibModalInstance.dismiss('cancel');
+	};
 
 
 
