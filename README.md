c5ea2c222427814157c085ddfd6f471497f6ac70
jasonbai <1105843831@qq.com>
2016-04-17 17:40:26 +0800

    实现最核心的功能，完善md文件

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# ngForm
+# Profile
+> 此SPA仅供学习、练习、娱乐  
+> 技术栈：AngularJs + Angular-Ui + Bootstrap.css + FontAwesome
+> 主要练习ng的表单，表单验证，表格的按钮添加，ngUI的tooltips, Bs的表格样式
+
+### Version
+```sh
+1.0.0
+```
+
+### Installation
+
+```sh
+$ git clone https://github.com/JasonBai007/myApp.git
+open your browser 
+```
+
+### Todos
+
+ - 暂无
+ 
+
+License
+----
+
+MIT
--- js/controller.js
+++ js/controller.js
@@ -7,11 +7,23 @@ ngForm.controller('myCtrl',['$scope',function($scope){
 	$scope.user = {};
 	$scope.records = [];
 	$scope.areas = ["海淀","朝阳","丰台","昌平","通州","大兴","亦庄","密云","顺义","房山"];
+	$scope.isEdit = false;
+	$scope.editIndex = 0;
 
 	// 提交当条记录
-	$scope.submit = function() {		
-		var item = angular.copy($scope.user);
-		$scope.records.push(item);
+	$scope.submit = function() {
+
+		// 得到表单的值
+		var NewRecord = angular.copy($scope.user);
+
+		// 首先判断是否启用编辑
+		if($scope.isEdit) {
+			$scope.records.splice($scope.editIndex,1,NewRecord);			
+			$scope.isEdit = false;  //编辑完记得复原全局变量
+		} else {
+			$scope.records.push(NewRecord);
+		}
+		// 清空表单	
 		$scope.user = {};
 	}
 
@@ -20,19 +32,20 @@ ngForm.controller('myCtrl',['$scope',function($scope){
 		$scope.user = {};
 	}
 
-	// 编辑记录
+	// 编辑记录【只有单击编辑时，$scope.isEdit 才为 true，其余时刻都为false】
 	$scope.edit = function ($index) {
-		var i = $index;
-		$scope.user = angular.copy($scope.records[i]);
+		$scope.isEdit = true;
+		$scope.editIndex = $index;
+		$scope.user = angular.copy($scope.records[$scope.editIndex]);
 	}
 
 	// 删除记录
 	$scope.del = function($index) {
 		var i = $index;
 		$scope.records.splice(i,1);
+		$scope.isEdit = false;
+		$scope.user = {};
 	}
-
-
 
 }]);
 
